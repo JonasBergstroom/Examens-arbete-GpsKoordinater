9581c44e99ef669a304b71570f900c951f814e30
jonasbergstrom <jontrian97@gmail.com>
2022-05-20 12:36:45 +0200

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/example/gpskoordinater/Greeting.kt
@@ -0,0 +1,7 @@
+package com.example.gpskoordinater
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/example/gpskoordinater/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.gpskoordinater
+
+expect class Platform() {
+    val platform: String
+}

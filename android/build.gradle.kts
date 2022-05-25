3e66ad8a0e363fb70e0800956438a0b6cb8b5ec4
jonasbergstrom <jontrian97@gmail.com>
2022-05-25 18:37:11 +0200

    Added "nav_graph" to be able to navigate to the "fragment_start"

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -24,4 +24,6 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.4.2")
 }

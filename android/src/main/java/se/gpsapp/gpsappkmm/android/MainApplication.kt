330d06760a3e42396a391f0ee68dc587d31d8e34
jonasbergstrom <jontrian97@gmail.com>
2022-05-28 23:16:59 +0200

    Added the MainApplication file that will handle the viewModel

---
+++ android/src/main/java/se/gpsapp/gpsappkmm/android/MainApplication.kt
@@ -0,0 +1,17 @@
+package se.gpsappkmm.gpsappkmm.android
+
+import android.app.Application
+import se.gpsappkmm.gpsappkmm.forms.FormViewModel
+import se.gpsapp.gpsappkmm.forms.forms.FormInfiltrations
+import se.gpsappkmm.gpsappkmm.forms.forms.FormType
+
+class MainApplication : Application() {
+    var formViewModel: FormViewModel =
+        FormViewModel(FormInfiltrations(FormType.Infiltration))
+
+    fun setViewModel(formType: FormType) {
+        formViewModel = when (formType) {
+            FormType.Infiltration -> FormViewModel(FormInfiltrations(FormType.Infiltration))
+        }
+    }
+}

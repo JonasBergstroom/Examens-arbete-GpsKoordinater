84649badb514639ecc23a67dbd6cf5e3731d2d7a
jonasbergstrom <jontrian97@gmail.com>
2022-05-29 16:06:30 +0200

    Added some dependencies with different implementation in the build gradle file

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,13 +1,15 @@
 plugins {
     id("com.android.application")
     kotlin("android")
+    id("kotlin-android")
+    id("androidx.navigation.safeargs")
 }
 
 android {
     compileSdk = 31
     defaultConfig {
         applicationId = "se.gpsapp.gpsappkmm.android"
-        minSdk = 23
+        minSdk = 21
         targetSdk = 31
         versionCode = 1
         versionName = "1.0"
@@ -17,6 +19,10 @@ android {
             isMinifyEnabled = false
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -24,6 +30,14 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.4.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.4.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.4.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+
+    implementation("androidx.activity:activity-ktx:1.5.0-alpha02")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
+    implementation("com.android.databinding:viewbinding:7.1.2")
+    implementation("androidx.core:core:1.7.0")
 }
